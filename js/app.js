7b0154e2f3dba8f4f1d0cd871564cf307a20a6d4
r4ulrr-rgb <r4ulrr@gmail.com>
2026-06-03 15:25:52 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-alert("APP FUNCIONANDO");
+const tickets = document.getElementById('tickets');
+
+for(let i=1;i<=10;i++){
+
+    const d=document.createElement('div');
+
+    d.className='ticket disponible';
+
+    d.textContent=String(i).padStart(3,'0');
+
+    tickets.appendChild(d);
+}
